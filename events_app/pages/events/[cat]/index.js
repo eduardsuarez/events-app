890525b0a7195239193b036b0bc7a5faf1a3d85d
Eduard Suárez <suareze205@gmail.com>
2023-03-15 17:20:47 -0500

    Next.js Pages and Build the pages

---
+++ events_app/pages/events/[cat]/index.js
@@ -0,0 +1,16 @@
+const EventsCatPage = () => {
+    return (
+        <div>
+            <h1>Events in London</h1>
+            <div>
+                <a href=""><img /> <h2>Event 1</h2></a>
+                <a href=""><img /> <h2>Event 2</h2></a>
+                <a href=""><img /> <h2>Event 3</h2></a>
+                <a href=""><img /> <h2>Event 4</h2></a>
+                <a href=""><img /> <h2>Event 5</h2></a>
+            </div>
+        </div>
+    )
+}
+
+export default EventsCatPage;
---
+++ events_app/pages/events/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const EventsPage = () => {
+
+  return (
+    <div>
+      <h1>Event Page</h1>
+      <div>
+        <a href="">
+          <img />
+          <h2> Events in London</h2>
+        </a>
+
+        <a href="">
+          <img />
+          <h2> Events in San Francisco</h2>
+        </a>
+
+        <a href="">
+          <img />
+          <h2> Events in Barcelona</h2>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default EventsPage;
+
+
