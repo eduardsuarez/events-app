a03bf13f440b599cf84bd4b63e4eaf05ebcca491
Eduard Suárez <suareze205@gmail.com>
2023-03-17 16:52:07 -0500

    Actualización

--- events_app/pages/about-us.js
+++ events_app/pages/about-us.js
@@ -1,7 +1,7 @@
 const AboutUsPage = () => {
   return (
     <div>
-        <h1>About us Page</h1>
+      <h1>About us Page</h1>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
       veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
@@ -10,6 +10,7 @@ const AboutUsPage = () => {
       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
       est laborum
     </div>
+    { console.log('Hola') }
   );
 };
 
